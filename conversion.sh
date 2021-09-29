ae262a0f059fa34f4f77f746557ea84fd0c981b7
Spencer Yeager <yeager.spencer@gmail.com>
2021-09-28 22:56:09 -0700

    works in current form

--- conversion.sh
+++ conversion.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Jupyter and wkhtmltopdf needed.
+
 echo "Converting notebook"
-ls
-mkdir hello
+
+jupyter nbconvert --execute --to html *.ipynb
+#converts to html. This retains image placement a bit better than 
+# direct conversion to PDF I've found
+
+wkhtmltopdf --enable-local-file-access *.html convert.pdf
+#converts to html. local access enabled to include pictures.
+
+rm *.html
+echo "Conversion complete"
+
